55f5694d082a0b88579c6e45888bed80609ca859
Martin Umpierrez <140851573+Martin-Umpierrez@users.noreply.github.com>
2026-05-20 20:53:32 -0300

    exeval_models
    Model_Data and documentation

---
+++ R/exeval_models.R
@@ -0,0 +1,16 @@
+#' Built-in pharmacokinetic models
+#'
+#' Curated population pharmacokinetic models available within the package.
+#'
+#' @format A data frame with 6 variables:
+#' \describe{
+#'   \item{Label}{Unique model identifier}
+#'   \item{Drug}{Drug name}
+#'   \item{Author}{First author}
+#'   \item{Year}{Publication year}
+#'   \item{Ref}{Name of Article}
+#'   \item{Model_code}{Model code as character string}
+#' }
+#'
+"exeval_models"
+
