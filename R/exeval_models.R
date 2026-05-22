36398463d535d3cceacd3a5fdf33708d5821de1d
Martin Umpierrez <140851573+Martin-Umpierrez@users.noreply.github.com>
2026-05-22 11:35:21 -0300

    exeval_model doc
    documentation on exeval_models uploaded in the package

--- R/exeval_models.R
+++ R/exeval_models.R
@@ -1,15 +1,26 @@
-#' Built-in pharmacokinetic models
+#' Built-in population PK/PKPD models
 #'
-#' Curated population pharmacokinetic models available within the package.
+#' Curated population pharmacokinetic (PK) and pharmacokinetic-pharmacodynamic
+#' (PKPD) models included in the package for external evaluation workflows.
+#'
+#' These models can be used directly in [exeval_ppk()] by supplying the
+#' corresponding \code{Label} as the \code{model} argument.
 #'
 #' @format A data frame with 6 variables:
 #' \describe{
-#'   \item{Label}{Unique model identifier}
-#'   \item{Drug}{Drug name}
-#'   \item{Author}{First author}
-#'   \item{Year}{Publication year}
-#'   \item{Ref}{Name of Article}
-#'   \item{Model_code}{Model code as character string}
+#'   \item{Label}{Unique model identifier used to reference the model within
+#'   the package.}
+#'
+#'   \item{Drug}{Drug associated with the model.}
+#'
+#'   \item{Author}{First author of the original publication.}
+#'
+#'   \item{Year}{Publication year.}
+#'
+#'   \item{Ref}{Reference title or citation for the original model publication.}
+#'
+#'   \item{Model_code}{Model code stored as a character string in
+#'   \pkg{mrgsolve} format.}
 #' }
 #'
 "exeval_models"
